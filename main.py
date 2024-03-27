58a7803af416c4d1fab865179c22c9b872b37aaa
jay_maurya <jpm12392@gmail.com>
2024-03-27 12:55:56 +0530

    changes

--- main.py
+++ main.py
@@ -63,24 +63,43 @@ from docx import Document
 # print_table_data(tables_data)
 # ++++++++++++++++++++++++++++++++++++++++++++++++
 
-def extract_table_column_names(file_path, keyword):
-    doc = Document(file_path)
-    for para in doc.paragraphs:
-        if keyword in para.text:
-            column_names = []
-            for table in doc.tables:
-                if len(table.rows) > 0:
-                    first_row = table.rows[0]
-                    for cell in first_row.cells:
-                        column_names.append(cell.text.strip())
-                    break
-            return column_names
+# def extract_table_column_names(file_path, keyword):
+#     doc = Document(file_path)
+#     for para in doc.paragraphs:
+#         if keyword in para.text:
+#             column_names = []
+#             for table in doc.tables:
+#                 if len(table.rows) > 0:
+#                     first_row = table.rows[0]
+#                     for cell in first_row.cells:
+#                         column_names.append(cell.text.strip())
+#                     break
+#             return column_names
 
-file_path = r'E:\python_project\python_docs\python_docs\todo_table.docx'
-filename = os.path.basename(file_path)
-print("Filename>>>>>>>>>>>>>>>:", filename)
-first_value = filename.split('_')[0]
-print('first_value>>>>', first_value)
-keyword = 'Applying K-means with no. clusters = 7'
-column_names = extract_table_column_names(file_path, keyword)
-print("Table Column Names:", column_names)
+# file_path = r'E:\python_project\python_docs\python_docs\todo_table.docx'
+# filename = os.path.basename(file_path)
+# print("Filename>>>>>>>>>>>>>>>:", filename)
+# first_value = filename.split('_')[0]
+# print('first_value>>>>', first_value)
+# keyword = 'Applying K-means with no. clusters = 7'
+# column_names = extract_table_column_names(file_path, keyword)
+# print("Table Column Names:", column_names)
+
+# from docx import Document
+
+def read_docx_table(docx_file):
+    doc = Document(docx_file)
+    table = doc.tables[0]  # Assuming there is only one table in the document
+    data = []
+    for row in table.rows:
+        row_data = []
+        for cell in row.cells:
+            row_data.append(cell.text)
+        data.append(row_data)
+    return data
+
+if __name__ == "__main__":
+    docx_file = r'E:\python_project\python_docs\python_docs\todo_table.docx' 
+    table_data = read_docx_table(docx_file)
+    for row in table_data:
+        print(row)
